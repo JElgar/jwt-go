66cef8f1d4fd6753295406c7fbdcfadcf57d1c68
James Elgar <jamesnelgar@gmail.com>
2019-10-15 09:21:54 +0100

    push

--- jwt.go
+++ jwt.go
@@ -3,6 +3,8 @@ package jwt
 import (
     b64 "encoding/base64"
     "log"
+    "crypto/rsa"
+    "fmt"
 )
 
 func EncodeB64URL(s string) string {
@@ -17,3 +19,43 @@ func DecodeB64URL(s string) string {
     return string(sD)
 }
 
+func GenerateSignature(headerAndPayload string) string{
+   signer, err := loadPrivateKey("private.pem")
+	if err != nil {
+		fmt.Errorf("signer is damaged: %v", err)
+	}
+
+	signed, err := signer.Sign([]byte(headerAndPayload))
+    fmt.Println(signed)
+}
+
+// loadPrivateKey loads an parses a PEM encoded private key file.
+func loadPublicKey(path string) (Unsigner, error) {
+        data, err := ioutil.ReadFile(path)
+
+        if err != nil {
+                return nil, err
+        }
+        return parsePublicKey(data)
+}
+
+// parsePublicKey parses a PEM encoded private key.
+func parsePrivateKey(pemBytes []byte) (Signer, error) {
+        block, _ := pem.Decode(pemBytes)
+        if block == nil {
+                return nil, errors.New("ssh: no key found")
+        }
+
+        var rawkey interface{}
+        switch block.Type {
+        case "RSA PRIVATE KEY":
+                rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+                if err != nil {
+                        return nil, err
+                }
+                rawkey = rsa
+        default:
+                return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+        }
+        return newSignerFromKey(rawkey)
+}
